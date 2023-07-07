196f2160337832f6a7ee96b96f2cbe7fc770a20e
Anusha G <95475948+Anusha0521@users.noreply.github.com>
2023-07-07 11:11:46 +0530

    Create Electrician.js

---
+++ src/routes/Electrician.js
@@ -0,0 +1,12 @@
+import RegisterForm from "../components/RegisterForm";
+import Footer from "../components/Footer";
+
+function Electrician(){
+    return(
+        <>
+        <RegisterForm/>
+        <Footer/>
+        </>
+    )
+}
+export default Electrician;
