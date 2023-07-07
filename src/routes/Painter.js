3c73667cb66b8b2744ebcc24da1b7d846226dbcf
Anusha G <95475948+Anusha0521@users.noreply.github.com>
2023-07-07 11:09:39 +0530

    Create Painter.js

---
+++ src/routes/Painter.js
@@ -0,0 +1,12 @@
+import RegisterForm from "../components/RegisterForm";
+import Footer from "../components/Footer";
+
+function Painter(){
+    return(
+        <>
+        <RegisterForm/>
+        <Footer/>
+        </>
+    )
+}
+export default Painter;
