dcaf29233038c23b6b148490733da8b0045d5cd5
Anusha G <95475948+Anusha0521@users.noreply.github.com>
2023-07-07 11:12:37 +0530

    Create Cleaner.js

---
+++ src/routes/Cleaner.js
@@ -0,0 +1,12 @@
+import RegisterForm from "../components/RegisterForm";
+import Footer from "../components/Footer";
+
+function Cleaner(){
+    return(
+        <>
+        <RegisterForm/>
+        <Footer/>
+        </>
+    )
+}
+export default Cleaner;
